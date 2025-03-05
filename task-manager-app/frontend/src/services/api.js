ddcee2945daa13326a2f4e58603ad3ebdb9decbc
Vasantha Mohan <40915013+mohanasgun@users.noreply.github.com>
2025-03-05 12:37:07 +0000

    Updated url

--- task-manager-app/frontend/src/services/api.js
+++ task-manager-app/frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://probable-space-system-w57vj4p6972gg5r-5000.app.github.dev/';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 console.log('API_URL:', API_URL);
 // Set up Axios instance
@@ -26,24 +26,34 @@ export const login = async (userData) => {
   return response.data;
 };
 
+export const logout = async (userData) => {
+  const response = await api.post('/api/users/logout', userData);
+  return response.data;
+};
+
 // Task API calls
 export const getTasks = async () => {
-  console.log('getTasks');
   const response = await api.get('/api/tasks');
   return response.data;
 };
 
-export const createTask = async (taskData) => {
+// Task API calls
+export const getTask = async (id) => {
+  const response = await api.get(`/api/tasks/${id}`);
+  return response.data;
+};
+
+export const storeTask = async (taskData) => {
   const response = await api.post('/api/tasks', taskData);
   return response.data;
 };
 
-export const updateTask = async (id, taskData) => {
+export const updateTaskItem = async (id, taskData) => {
   const response = await api.put(`/api/tasks/${id}`, taskData);
   return response.data;
 };
 
-export const deleteTask = async (id) => {
+export const removeTask = async (id) => {
   const response = await api.delete(`/api/tasks/${id}`);
   return response.data;
 };
